607998397c26c046f7a46cc08a376fe86b0cd63e
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-07-07 15:01:04 +0200

    add date validation

--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PromotionsController.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/controllers/PromotionsController.java
@@ -4,12 +4,12 @@ import org.pizzeria.java.spring_la_mia_pizzeria_crud.model.Promotion;
 import org.pizzeria.java.spring_la_mia_pizzeria_crud.repo.PromotionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.model.Model;
 import jakarta.validation.Valid;
 
 @Controller
@@ -21,15 +21,25 @@ public class PromotionsController {
 
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("promotion") Promotion promotion, BindingResult bindingResult,
-            Model model) {
+            Model model) throws Exception {
+        try {
+            if (bindingResult.hasErrors()) {
+                return "promotions/create";
+            }
 
-        if (bindingResult.hasErrors()) {
+            if (promotion.getStartDate().isAfter(promotion.getEndDate())) {
+                throw new Exception("Start date cannot be after end date");
+            }
+
+            promotionRepo.save(promotion);
+
+            return "redirect:/menu/pizza/" + promotion.getPizza().getId();
+
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
             return "promotions/create";
         }
 
-        promotionRepo.save(promotion);
-
-        return "redirect:/menu/pizza/" + promotion.getPizza().getId();
     }
 
 }
